a936c5cb82a2d0574af233554c4b0063ee7bb533
Egor Bronnikov <52889537+endygamedev@users.noreply.github.com>
2020-02-24 17:39:33 +0300

    Delete get_parity.py

--- get_parity.py
+++
@@ -1,8 +0,0 @@
-def parity(num):
-    return "Чётное" if num%2 == 0 else "Нечётное"
-
-input_num = input("Введите число: ")
-if input_num:
-    print(parity(int(input_num)))
-else:
-    print("Введите число!!!")
