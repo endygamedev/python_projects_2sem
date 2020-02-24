8d54149cacb37ad1b99ebb594cabc757e2358605
Egor Bronnikov <52889537+endygamedev@users.noreply.github.com>
2020-02-24 17:39:24 +0300

    Delete fun1.py

--- fun1.py
+++
@@ -1,8 +0,0 @@
-def fun(x):
-    return x**2 if -2.4 <= x <= 5.7 else 4
-
-input_x = input("Введите значения перменной x: ")
-if input_x:
-    print(fun(float(input_x)))
-else:
-    print("Введите число!!!")
